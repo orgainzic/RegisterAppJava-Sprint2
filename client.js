ccc15a945f44261a473d84fa19eb6e5824e627ce
Alex Lim <syl002@uark.edu>
2020-04-02 22:20:36 -0500

    Created basic client html and js files, for use later

---
+++ client.js
@@ -0,0 +1,34 @@
+
+
+/*client.js file, onclick functions
+
+
+
+function addProduct(event)	{
+
+
+}
+
+
+function removeProduct(event)	{
+
+
+
+}
+
+
+function updateQuantity(event)	{
+
+
+}
+
+function submitTransaction(event)	{
+
+
+}
+
+function cancelTransaction (event)	{
+
+}
+
+*/
