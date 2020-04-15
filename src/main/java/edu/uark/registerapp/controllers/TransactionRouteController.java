27d45fa54ec51b0b3ba3829db39f8e64805eec17
TheFlyIsASpy <nicolassherid@gmail.com>
2020-04-15 18:52:36 -0500

    added second section for searching

--- src/main/java/edu/uark/registerapp/controllers/TransactionRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/TransactionRouteController.java
@@ -20,7 +20,7 @@ import edu.uark.registerapp.models.enums.EmployeeClassification;
 @RequestMapping(value = "/transaction")
 public class TransactionRouteController extends BaseRouteController {
 	@RequestMapping(method = RequestMethod.GET)
-	public ModelAndView start(
+	public ModelAndView landing(
 		@RequestParam final Map<String, String> queryParameters,
 		final HttpServletRequest request
 	) {
@@ -44,5 +44,33 @@ public class TransactionRouteController extends BaseRouteController {
 		*/
 
 			return modelAndView;
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ModelAndView searched(
+		@RequestParam final Map<String, String> queryParameters,
+		final HttpServletRequest request
+	) {
+        ModelAndView modelAndView =
+			this.setErrorMessageFromQueryString(
+				new ModelAndView(ViewNames.TRANSACTION.getViewName()),
+				queryParameters);
+
+		System.out.println(queryParameters);
+		/*
+				try {
+				modelAndView.addObject(
+					//ViewModelNames.PRODUCTS.getValue(),  change to checkout viewmodel or use this one
+					//this.productsQuery.execute());   change to checking the cart command
+			} catch (final Exception e) {
+				modelAndView.addObject(
+					ViewModelNames.ERROR_MESSAGE.getValue(),
+					e.getMessage());
+				modelAndView.addObject(
+					//ViewModelNames.PRODUCTS.getValue(), change to checkout viewmodel or use this one
+					//(new Product[0]));  change to checkout model or use this one
+			}
+		*/
+		return modelAndView;
 	}
 }
