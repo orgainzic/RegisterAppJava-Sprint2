06ae7c8ac35d5f60bdfbdb3924379e7e456cb7e2
TheFlyIsASpy <nicolassherid@gmail.com>
2020-04-15 20:36:09 -0500

    Finished the routing for populating a template with partial search

--- src/main/java/edu/uark/registerapp/controllers/TransactionRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/TransactionRouteController.java
@@ -5,14 +5,17 @@ import java.util.Optional;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import edu.uark.registerapp.commands.transactions.ProductSearchByPartialLookupCode;
 import edu.uark.registerapp.controllers.enums.ViewModelNames;
 import edu.uark.registerapp.controllers.enums.ViewNames;
+import edu.uark.registerapp.models.api.Product;
 import edu.uark.registerapp.models.entities.ActiveUserEntity;
 import edu.uark.registerapp.models.enums.EmployeeClassification;
 
@@ -43,20 +46,28 @@ public class TransactionRouteController extends BaseRouteController {
 				queryParameters);
 
 		System.out.println(queryParameters);
-		/*
-				try {
+
+		System.out.println(queryParameters.get("searchProduct"));
+		
+		searchByPartialLookup.setPartialLookupCode(queryParameters.get("searchProduct"));
+		Product[] products = searchByPartialLookup.execute();
+
+			try {
 				modelAndView.addObject(
-					//ViewModelNames.PRODUCTS.getValue(),  change to checkout viewmodel or use this one
-					//this.productsQuery.execute());   change to checking the cart command
+					ViewModelNames.PRODUCTS.getValue(),
+					products);
 			} catch (final Exception e) {
 				modelAndView.addObject(
 					ViewModelNames.ERROR_MESSAGE.getValue(),
 					e.getMessage());
 				modelAndView.addObject(
-					//ViewModelNames.PRODUCTS.getValue(), change to checkout viewmodel or use this one
-					//(new Product[0]));  change to checkout model or use this one
+					ViewModelNames.PRODUCTS.getValue(),
+					(new Product[0]));
 			}
-		*/
+	
 		return modelAndView;
 	}
+
+	@Autowired
+	private ProductSearchByPartialLookupCode searchByPartialLookup;
 }
