ae679d7e6dd3b9b2ae44550f753f33938e523002
mr-christian-thompson <60154440+mr-christian-thompson@users.noreply.github.com>
2020-04-15 23:10:53 -0500

    Search form attempt

---
+++ src/main/java/edu/uark/registerapp/controllers/TransactionRouteController.java
@@ -0,0 +1,48 @@
+package edu.uark.registerapp.controllers;
+
+import java.util.Map;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import edu.uark.registerapp.controllers.enums.ViewModelNames;
+import edu.uark.registerapp.controllers.enums.ViewNames;
+import edu.uark.registerapp.models.entities.ActiveUserEntity;
+import edu.uark.registerapp.models.enums.EmployeeClassification;
+
+@Controller
+@RequestMapping(value = "/transaction")
+public class TransactionRouteController extends BaseRouteController {
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView start(
+		@RequestParam final Map<String, String> queryParameters,
+		final HttpServletRequest request
+	) {
+        ModelAndView modelAndView =
+			this.setErrorMessageFromQueryString(
+				new ModelAndView(ViewNames.TRANSACTION.getViewName()),
+				queryParameters);
+		/*
+				try {
+				modelAndView.addObject(
+					//ViewModelNames.PRODUCTS.getValue(),  change to checkout viewmodel or use this one
+					//this.productsQuery.execute());   change to checking the cart command
+			} catch (final Exception e) {
+				modelAndView.addObject(
+					ViewModelNames.ERROR_MESSAGE.getValue(),
+					e.getMessage());
+				modelAndView.addObject(
+					//ViewModelNames.PRODUCTS.getValue(), change to checkout viewmodel or use this one
+					//(new Product[0]));  change to checkout model or use this one
+			}
+		*/
+
+			return modelAndView;
+	}
+}
---
+++ src/main/resources/static/scripts/transaction.js
@@ -0,0 +1,90 @@
+/*
+document.addEventListener("DOMContentLoaded", function(event) {
+    getaddButton().addEventListener(
+        "click", () => {addProduct;});
+    getupdateButton().addEventListener(
+        "click", () => {updateQuantity;});
+    getremoveButton().addEventListener(
+        "click", () => {removeProduct;});
+    getcheckoutButton().addEventListener(
+        "click", () => {submitTransaction;});
+    getcancelButton().addEventListener(
+        "click", () => {cancelTransaction;});
+});
+*/
+
+
+function getClickedListItemElement(target) {
+    let clickedElement = target;
+    while (clickedElement.tagName !== "LI") {
+        clickedElement = clickedElement.parentElement;
+    }
+    return clickedElement;
+}
+
+function addProduct(event) {
+    const unorderedListElement = document.getElementById("productsListing");
+    const nextEntryId = (unorderedListElement.childElementCount + 1).toString();
+    const listItemElement = document.createElement("li");
+    listItemElement.addEventListener("click", removeProduct);
+    const lookupCodeDisplayElement = document.createElement("span");
+    lookupCodeDisplayElement.innerHTML = ("Product Lookup Code " + nextEntryId);
+    lookupCodeDisplayElement.classList.add("lookupCodeDisplay");
+    listItemElement.appendChild(lookupCodeDisplayElement);
+    listItemElement.appendChild(
+        document.createElement("br"));
+    const entryIdDisplayElement = document.createElement("span");
+    entryIdDisplayElement.innerHTML = ("\u00A0\u00A0" + nextEntryId);
+    listItemElement.appendChild(entryIdDisplayElement);
+    unorderedListElement.appendChild(listItemElement);
+}
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("addButton")
+        .addEventListener("click", addProduct);
+    const listItemElements = document.getElementById("productsListing")
+        .querySelectorAll("li");
+    for (let i = 0; i < listItemElements.length; i++) {
+        listItemElements[i].addEventListener("click", removeProduct);
+    }
+});
+/*
+function updateQuantity(event) {
+
+}
+*/
+
+function removeProduct(event) {
+    const unorderedListElement = document.getElementById("productsListing");
+    unorderedListElement.removeChild(
+        getClickedListItemElement(event.target));
+}
+/*
+function submitTransaction(event) {
+
+}
+function cancelTransaction(event) {
+
+}
+
+// Getters and setters
+
+function getaddButton() {
+    return document.getElementById("addButton");
+}
+
+function getupdateButton() {
+    return document.getElementById("updateButton");
+}
+
+function getremoveButton() {
+    return document.getElementById("removeButton");
+}
+
+function checkoutButton() {
+    return document.getElementById("checkoutButton");
+}
+
+function cancelButton() {
+    return document.getElementById("cancelButton");
+}
+*/
