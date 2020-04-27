e5ffd25014e2ca8dc0fab3ea10fa1b4f054bcb60
Gunner L <zgunnerl1@icloud.com>
2020-04-26 23:55:42 -0500

    Updated Product API to support prices

--- src/main/java/edu/uark/registerapp/models/api/Product.java
+++ src/main/java/edu/uark/registerapp/models/api/Product.java
@@ -40,6 +40,17 @@ public class Product extends ApiResponse {
 		return this;
 	}
 
+	private long price;
+
+	public long getPrice() {
+		return this.price;
+	}
+
+	public Product setPrice(final long price) {
+		this.price = price;
+		return this;
+	}
+
 	private String createdOn;
 
 	public String getCreatedOn() {
@@ -62,6 +73,7 @@ public class Product extends ApiResponse {
 		super();
 
 		this.count = -1;
+		this.price = -1;
 		this.id = new UUID(0, 0);
 		this.lookupCode = StringUtils.EMPTY;
 
@@ -73,6 +85,7 @@ public class Product extends ApiResponse {
 
 		this.id = productEntity.getId();
 		this.count = productEntity.getCount();
+		this.price = productEntity.getPrice();
 		this.lookupCode = productEntity.getLookupCode();
 
 		this.setCreatedOn(productEntity.getCreatedOn());
