d2ab04eaf3d058ee0683eda837babc4982a691b8
Gunner L <zgunnerl1@icloud.com>
2020-04-27 05:21:38 -0500

    Created Command to update the quantites based upon quantities in the "cart". To be used during checkout.

---
+++ src/main/java/edu/uark/registerapp/commands/transactions/UpdateTransactionEntryQuantities.java
@@ -0,0 +1,59 @@
+package edu.uark.registerapp.commands.transactions;
+
+import edu.uark.registerapp.commands.ResultCommandInterface;
+import edu.uark.registerapp.commands.VoidCommandInterface;
+import edu.uark.registerapp.commands.exceptions.NotFoundException;
+import edu.uark.registerapp.commands.exceptions.UnprocessableEntityException;
+import edu.uark.registerapp.models.api.TransactionEntry;
+import edu.uark.registerapp.models.entities.TransactionEntryEntity;
+import edu.uark.registerapp.models.repositories.TransactionEntryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+public class UpdateTransactionEntryQuantities implements ResultCommandInterface<TransactionEntry[]> {
+    @Transactional
+    @Override
+    public TransactionEntry[] execute() {
+
+        this.validateProperties();
+
+        for (TransactionEntry transactionEntry : this.transactionEntries) {
+            Optional<TransactionEntryEntity> transactionEntryEntity =
+                    this.transactionEntryRepository.findByTransactionIdAndProductId(
+                            transactionEntry.getTransactionId(),
+                            transactionEntry.getProductId());
+            if (!transactionEntryEntity.isPresent()) { // No record with the associated record Id was in database
+                throw new NotFoundException("TransactionEntry");
+            }
+
+            // Update quantity
+            transactionEntryEntity.get().setQuantity(transactionEntry.getQuantity());
+
+            // Write, via an UPDATE, any changes to the database
+            this.transactionEntryRepository.save(transactionEntryEntity.get());
+        }
+
+        return this.transactionEntries;
+    }
+
+    // Helper Methods
+    private void validateProperties() {
+        if (this.transactionEntries.equals(null)) {
+            throw new UnprocessableEntityException("transactionEntries");
+        }
+    }
+
+    // Properties
+    private TransactionEntry[] transactionEntries;
+    public TransactionEntry[] getTransactionEntries() { return this.transactionEntries; }
+    public UpdateTransactionEntryQuantities setTransactionEntries(final TransactionEntry[] transactionEntries) {
+        this.transactionEntries = transactionEntries;
+        return this;
+    }
+
+
+    @Autowired
+    private TransactionEntryRepository transactionEntryRepository;
+}
