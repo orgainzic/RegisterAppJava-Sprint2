f1d0a822bfe457b417d9a800f47c29c1697b0ffb
Gunner L <zgunnerl1@icloud.com>
2020-04-28 21:17:51 -0500

    Added validateProperties helper method to RemoveProductFromTransaction

--- src/main/java/edu/uark/registerapp/commands/transactions/RemoveProductFromTransaction.java
+++ src/main/java/edu/uark/registerapp/commands/transactions/RemoveProductFromTransaction.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import edu.uark.registerapp.commands.VoidCommandInterface;
 import edu.uark.registerapp.commands.exceptions.NotFoundException;
+import edu.uark.registerapp.commands.exceptions.UnprocessableEntityException;
 import edu.uark.registerapp.models.entities.TransactionEntryEntity;
 import edu.uark.registerapp.models.repositories.TransactionEntryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,8 @@ public class RemoveProductFromTransaction implements VoidCommandInterface {
     @Transactional
     @Override
     public void execute() {
+        this.validateProperties();
+
         final Optional<TransactionEntryEntity> transactionEntryEntity =
                 this.transactionEntryRepository.findById(this.transactionEntryId);
         if (!transactionEntryEntity.isPresent()) { // No record with the associated transaction ID and product ID exists in the database
@@ -23,6 +26,13 @@ public class RemoveProductFromTransaction implements VoidCommandInterface {
         }
 
         this.transactionEntryRepository.delete(transactionEntryEntity.get());
+    }
+
+    // Helper Methods
+    private void validateProperties() {
+        if (this.transactionEntryId.equals(null)) {
+            throw new UnprocessableEntityException("transactionEntryId");
+        }
     }
 
     // Properties
