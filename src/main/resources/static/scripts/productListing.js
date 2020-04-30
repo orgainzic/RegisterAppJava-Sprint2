f4624715b390eb19ae8eea33c8df00b936daef0f
mr-christian-thompson <60154440+mr-christian-thompson@users.noreply.github.com>
2020-04-29 20:48:06 -0500

    Attempt 15: Added back button to productListing page to the mainMenu page

--- src/main/resources/static/scripts/productListing.js
+++ src/main/resources/static/scripts/productListing.js
@@ -1,10 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
 	const productListElements = document.getElementById("productsListing").children;
+	document.getElementById("backButtonImage")
+		.addEventListener("click", backToMainClick);
 
 	for (let i = 0; i < productListElements.length; i++) {
 		productListElements[i].addEventListener("click", productClick);
 	}
 });
+
+//Back to Main Menu Page
+function backToMainClick() {
+	window.location.replace("/mainMenu");
+	
+}
+//End to Main Menu Page
 
 function findClickedListItemElement(clickedTarget) {
 	if (clickedTarget.tagName.toLowerCase() === "li") {
